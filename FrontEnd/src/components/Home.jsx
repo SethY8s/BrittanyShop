a8d25e46cf8ff9c82b2b65af790f0444c0ae2b6d
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2022-04-09 20:58:54 -0400

    home comp now knows if it is from checkout

--- FrontEnd/src/components/Home.jsx
+++ FrontEnd/src/components/Home.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Intro from './intro/Intro'
 import Products from './product/Products'
 
 export default function Home() {
+
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    // Check to see if this is a redirect back from Checkout
+    const query = new URLSearchParams(window.location.search);
+
+
+    if (query.get('canceled')) {
+      setMessage(
+        "Order canceled -- continue to shop around and checkout when you're ready."
+      );
+      console.log(message);
+    }
+  }, [message]);
+
+
   return (
       <>
     <Intro />
