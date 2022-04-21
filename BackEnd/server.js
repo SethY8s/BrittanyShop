a58d46f050d976a1d8415f28cb84cbdad3cd6626
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2022-04-21 15:19:46 -0400

    make it better

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -13,9 +13,6 @@ app.use(cors());
 const PORT = process.env.PORT || 4000;
 
 
-// const YOUR_DOMAIN = 'http://localhost:4242';
-
-
 const storeItems = new Map([
   [1, { price: 'price_1KnhrqEjQbs32fBJCfu2LmC9', quantity: 1 }],
   [2, { price: 'price_1KnhxoEjQbs32fBJdPIYcM8J', quantity: 1 }],
